33661adebe04224477f14167ed329c798d027346
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-06-29 22:39:47 +0300

    finish task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
- 
+
+const gallery = document.querySelector('.gallery');
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const liImg = galleryItems
+  .map(
+    elem => `<li class="gallery_item"><a class="gallery__link" href="${elem.original}"><img class="gallery__image"
+      src="${elem.preview}"
+      alt="${elem.description}"/></a></li>`
+  )
+  .join('');
+// Додавання розмітки в HTML.
+gallery.innerHTML = liImg;
+// ініціалізація SimpleLightbox
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
 console.log(galleryItems);
