5d37d375ed51f49a29d919258fe14293776e4184
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-07-18 23:16:56 +0300

    storadge-function

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,26 @@
 import Vimeo from '@vimeo/player';
 import LodashTrotle from 'lodash.throttle';
-
+import {save, load} from "./02-videostorage.js"
+const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+let timeupdate = {
+    duration: 61.857,
+    percent: 0.049,
+    seconds: 3.034,
+  }
 const onPlay = function (data) {
+  save(STORAGE_KEY, value);
+
   // data is an object containing properties specific to that event
 };
 
---
+++ src/js/02-videostorage.js
@@ -0,0 +1,20 @@
+const save = (key, value) => {
+  try {
+    const searilizedState = JSON.stringify(value);
+    localStorage.setItem(key, searilizedState);
+  } catch (error) {
+    console.error('Set state error:', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const searilizedState = localStorage.getItem(key);
+    return searilizedState === null ? undefined : JSON.parse(searilizedState);
+  } catch (error) {
+    console.error('Get state error:', error.message);
+  }
+};
+
+export default { save, load };
+
