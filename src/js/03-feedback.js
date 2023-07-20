bce4bd34d32b45fd563ce55793417c4ad09eddfb
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-07-20 18:07:12 +0300

    feedback_start

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+import LodashThrottle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const arrStorage = {
+    email: "email",
+    message: "message",
+};
+
+form.addEventListener("click", handleForm);
+function handleForm(event) {
+    console.log("target:", event.target);
+    console.log("currentTarget:", event.currentTarget);
+}
